e6d0dbdd1b758cb937e2b9c60e13a8d7babc4a62
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-07-01 14:54:04 +0530

    Set Theme

--- app/src/main/java/in/abhisheksaxena/gettaskdone/ui/MainActivity.kt
+++ app/src/main/java/in/abhisheksaxena/gettaskdone/ui/MainActivity.kt
@@ -3,10 +3,13 @@ package `in`.abhisheksaxena.gettaskdone.ui
 
 import `in`.abhisheksaxena.gettaskdone.R
 import `in`.abhisheksaxena.gettaskdone.data.db.local.TaskDatabase
+import `in`.abhisheksaxena.gettaskdone.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +19,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         navController = findNavController(R.id.default_navHost_fragment)
 
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        setSupportActionBar(binding.toolbar)
+
+        NavigationUI.setupActionBarWithNavController(this, navController )
     }
 
     override fun onSupportNavigateUp(): Boolean {
