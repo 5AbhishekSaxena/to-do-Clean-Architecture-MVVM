86a2f01816078a1a60105a5900b8459adc00e60e
5abhisheksaxena <5abhisheksaxena@gmail.com>
2020-07-09 17:48:43 +0530

    Add title character limit constant.

--- app/src/main/java/in/abhisheksaxena/gettaskdone/util/Constants.kt
+++ app/src/main/java/in/abhisheksaxena/gettaskdone/util/Constants.kt
@@ -8,12 +8,15 @@ package `in`.abhisheksaxena.gettaskdone.util
 
 class Constants {
     interface MESSAGE {
-
         companion object {
             const val ADD_TASK_OK = 1
             const val UPDATE_TASK_OK = 2
             const val UPDATE_TASK_NOT_OK = 3
             const val DELETE_TASK_OK = 4
         }
+    }
+
+    companion object {
+        val TITLE_CHARACTER_LIMIT = 75
     }
 }
